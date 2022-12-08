daf355dd985ed0d84a5f313614bb8c53750ece71
Glitch (flower-cookie-sunflower) <none>
2022-12-08 19:16:56 +0000

    💁⛄️ Checkpoint ./src/server.js:27621106/136 ./Events/Init/ready.js:27621106/3057

--- src/server.js
+++ src/server.js
@@ -221,7 +221,7 @@ let commands = [];
 client.commands = new Collection();
 readdirSync("./Commands").forEach((folder) => {
   readdirSync(`./Commands/${folder}`).forEach((file) => {
-    const command = require(`./Commands/${folder}/${file}`);
+    const command = require(`../Commands/${folder}/${file}`);
     client.commands.set(command.data.name, command);
     if (command.data.name !== "help") commands.push(command.data.toJSON());
   });
@@ -230,7 +230,7 @@ module.exports = commands;
 
 readdirSync("./Events").forEach((folder) => {
   readdirSync(`./Events/${folder}`).forEach((file) => {
-    const event = require(`./Events/${folder}/${file}`);
+    const event = require(`../Events/${folder}/${file}`);
     client.on(file.split(".")[0], event.bind(null, client));
   });
 });
