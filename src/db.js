672b24f54a4195bf2601ff10717b6b973c79a3b1
Glitch (flower-cookie-sunflower) <none>
2022-12-11 15:33:21 +0000

    🎥🖼 Checkpoint ./src/db.js:27621106/1028

--- src/db.js
+++ src/db.js
@@ -1,29 +1,3 @@
-/*const mongoose = require('mongoose');
-
-mongoose.connect(
-  process.env.MONGO_URI || 'mongodb+srv://admin69:arrastx_membership@arras0tx0membership.d9fx6.mongodb.net/arrastx_membership?retryWrites=true&w=majority', { 
-    useNewUrlParser: true, 
-    useCreateIndex: true 
-  }
-)
-
-module.exports = mongoose; */
-// here is where we connect to the database
-/*
-const mongoose = require('mongoose');
-const mongodb_URI =
-  "mongodb+srv://LB:Lionim4321@cluster0.w6jgm.mongodb.net/cluster0?retryWrites=true&w=majority"; //process.env.MONGODB_URI
-//const mongodb_URI = 'mongodb+srv://tjhickey:WcaLKkT3JJNiN8dX@cluster0.kgugl.mongodb.net/atlasAuthDemo?retryWrites=true&w=majority' //process.env.MONGODB_URI
-const dbURL = mongodb_URI;
-mongoose.connect(dbURL, {
-    useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true  });
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-  console.log("we are connected!!!");
-});
-module.exports = mongoose; */
 const mongoose = require('mongoose');
 
 mongoose.connect(
