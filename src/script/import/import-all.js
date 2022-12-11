b05ceefdb6605bf9e09d3c79ad53c80177f5b94e
Glitch (flower-cookie-sunflower) <none>
2022-12-11 10:03:52 +0000

    🎢👏 Checkpoint ./src/script/import/import-all.js:27621106/538

---
+++ src/script/import/import-all.js
@@ -0,0 +1,14 @@
+const settings = require("../../../settings.json");
+const roles = require("../../../roles.json");
+const users = require("../../../users.json");
+const db = require("../../db");
+const Role = require("../../models/role");
+const User = require("../../models/user");
+const Settings = require("../../models/settings");
+
+// This file is entirely made by attacker and is used to import all default settings in 1 file instead of 3.
+
+(async function() {
+
+
+})();
